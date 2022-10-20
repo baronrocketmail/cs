90b293bc3e01aae36c43c43b10dd96ce1f29c712
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 15:29:03 -0700

    initial3

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import NavLinks from "./Components/NavLinks";
+import {useState, useEffect} from "react";
+import {fetchPropertyInfoObj, fetchUnpaidObjArray} from "./api/dataFetching.mjs";
 
 export default function Home() {
-  return (
-      <div>
 
-       <NavLinks objArry = {[{name: "dsffsdf", url:"/sd"}]}/>
+    const [unpaidObjArray, setUnpaidObjArray] = useState([])
 
-      </div>
-  )
+    useEffect(() => {
+        fetchUnpaidObjArray().then( unpaidObjArray => {
+                fetchPropertyInfoObj(). then( propertyInfoObj=> {
+                    var objArray = [];
+                    objArray.push(...[{name: propertyInfoObj.name, url: "/"}, {name: "autopay", url: "/autopay"}]);
+                    objArray.push(...unpaidObjArray)
+                    objArray.push(...[{name: "...", url:"/log"}])
+                    setUnpaidObjArray(objArray);
+                })
+            }
+        );
+    }, [])
+
+
+    return(
+        <div>
+            <NavLinks objArry = {unpaidObjArray}/>
+        </div>
+    )
 }
