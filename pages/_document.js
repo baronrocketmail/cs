d658b1869ebc42e8b0ae262d5b45cf5aee71fae1
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 16:58:36 -0700

    initial

--- pages/_document.js
+++ pages/_document.js
@@ -5,25 +5,11 @@ export default function Document() {
         <Html>
             <Head>
                 <link
-                    rel = "preload"
-                    href="../../18572live2/public/SF-Pro.woff2"
-                    as = "font"
-                    type = "font/woff2"
-                    crossOrigin={"anonymous"}
-                />
-                <link
-                    rel = "preload"
-                    href="../../18572live2/public/SF-Pro.woff"
-                    as = "font"
-                    type = "font/woff"
-                    crossOrigin={"anonymous"}
-                />
-                <link
-                    rel = "preload"
-                    href="../../18572live2/public/SF-Pro.ttf"
-                    as = "font"
-                    type = "font/truetype"
-                    crossOrigin={"anonymous"}
+                    rel="preload"
+                    href="../public/SF-Pro.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin="anonymous"
                 />
             </Head>
             <body>
