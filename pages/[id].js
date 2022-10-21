b1e6348f0fa94e01a8722f5ef707b2df8f8717f3
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 17:39:23 -0700

    intial2

--- pages/[id].js
+++ pages/[id].js
@@ -2,6 +2,6 @@ import NavLinks from "./Components/NavLinks";
 
 export default function Specific(){
     return(
-        <NavLinks objArry ={[{name:"18572 cull canyon",url: "/"  }]}/>
+        <NavLinks objArry ={[{name:"<------",url: "/"  }]}/>
     )
 }
