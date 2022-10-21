f0a969e43f7dacfee2e335d93b63c8dc68c12a11
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 17:35:16 -0700

    inital2

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,14 @@ export default function Document() {
     return (
         <Html>
             <Head>
-            <link rel="preload" href="/public/SF-Pro.woff2.woff2" as="font" type="font/woff2" crossOrigin/>
-            <link rel="preload" href="/public/SF-Pro.woff2.ttf" as="font" type="font/truetype" crossOrigin/>
-            <link rel="preload" href="/public/SF-Pro.woff2.woff2" as="font" crossOrigin="anonymous"/>
-            <link rel="preload" href="https://storage.googleapis.com/ponn/SF-Pro.woff" as="font" crossOrigin="anonymous"/>
+            {/*<link rel="preload" href="/public/SF-Pro.woff2.woff2" as="font" type="font/woff2" crossOrigin/>*/}
+            {/*<link rel="preload" href="/public/SF-Pro.woff2.ttf" as="font" type="font/truetype" crossOrigin/>*/}
+            {/*<link rel="preload" href="/public/SF-Pro.woff2.woff2" as="font" crossOrigin="anonymous"/>*/}
+            {/*<link rel="preload" href="https://storage.googleapis.com/ponn/SF-Pro.woff" as="font" crossOrigin="anonymous"/>*/}
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+                              rel="stylesheet"/>
 
 
             </Head>
